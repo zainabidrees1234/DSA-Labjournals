b9261e9b46d21933876a844a9782384c386a4cf9
Zainab Idrees <zainabidrees57@gmail.com>
2025-03-02 16:58:51 +0500

    Labtask 4: Queue & Deque Implementation

--- Week4/deque.cpp
+++ Week4/deque.cpp
@@ -1,7 +1,7 @@
 #include "deque.h"
 int main() {
-    string str = "madam";
-    cout << (isPalindrome(str) ? "Palindrome" : "Not Palindrome") << endl;
+    string word = "madam";
+    cout << (isPalindrome(word) ? "Palindrome" : "Not Palindrome") << endl;
     return 0;
 }
 
